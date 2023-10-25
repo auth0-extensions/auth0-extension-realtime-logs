81f1e3fa245f7fff7b23a9541c4f6eea9cdc4585
Eddie Zhou <eddie.zhou@auth0.com>
2023-10-25 10:43:48 -0700

    feat: remove hard-coded logic for classic environments

--- utils.js
+++ utils.js
@@ -1,29 +1,5 @@
 
 function resolveWebtaskAPIHost(host, context) {
-  if (host.indexOf('.us.webtask.io') > 0) {
-    return 'https://sandbox.it.auth0.com';
-  }
-
-  if (host.indexOf('.au.webtask.io') > 0) {
-    return 'https://sandbox-au.it.auth0.com';
-  }
-
-  if (host.indexOf('.eu.webtask.io') > 0) {
-    return 'https://sandbox-eu.it.auth0.com';
-  }
-
-  if (host.indexOf('.us8.webtask.io') > 0) {
-    return 'https://sandbox8-us.it.auth0.com';
-  }
-
-  if (host.indexOf('.au8.webtask.io') > 0) {
-    return 'https://sandbox8-au.it.auth0.com';
-  }
-
-  if (host.indexOf('.eu8.webtask.io') > 0) {
-    return 'https://sandbox8-eu.it.auth0.com';
-  }
-
   var wtUrl = context.secrets.WT_URL;
 
   if (wtUrl && wtUrl.indexOf('api/run') >= 0 ) {
@@ -34,5 +10,5 @@ function resolveWebtaskAPIHost(host, context) {
 }
 
 module.exports = {
-  resolveWebtaskAPIHost: resolveWebtaskAPIHost
+  resolveWebtaskAPIHost
 }
